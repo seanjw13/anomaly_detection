739bb12393179d84fc348081cd75ce77d3a00311
Sean Wilkinson <sean.wilkinson@databricks.com>
2022-04-26 14:36:52 -0700

    Adding docker-compose file and example DAG.

---
+++ airflow/dags/anomaly_detection_dag.py
@@ -0,0 +1,57 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from airflow.providers.databricks.operators.databricks_repos import (
+    DatabricksReposUpdateOperator,
+)
+
+default_args = {
+    'owner': 'airflow',
+    'databricks_conn_id': 'databricks',
+}
+
+with DAG(
+    dag_id='anomaly_detection_dag',
+    start_date=datetime(2022, 4, 25),
+    schedule_interval='@yearly',
+    default_args=default_args,
+    tags=['example'],
+    catchup=False,
+) as dag:
+
+    # Example of updating a Databricks Repo to the latest code
+    repo_path = "/Repos/sean.wilkinson@databricks.com/anomaly_detection"
+    update_repo = DatabricksReposUpdateOperator(task_id='update_repo', repo_path=repo_path, branch="main")
+
+    notebook_task_params = {
+        'new_cluster': {
+            'spark_version': '10.4.x-cpu-ml-scala2.12',
+            'node_type_id': 'n1-standard-4',
+            'num_workers': 1,
+        },
+        'notebook_task': {
+            'notebook_path': f'{repo_path}/hello_airflow',
+        },
+    }
+
+    notebook_task = DatabricksSubmitRunOperator(task_id='notebook_task', json=notebook_task_params)
+
+    (update_repo >> notebook_task)
